138950a44a9ef3a4d0c5023ef8a9947355d9100e
Michael Solati <mkslt04@gmail.com>
2016-12-03 14:02:10 -0500

    fixes

--- src/app/home-map/home-map.component.ts
+++ src/app/home-map/home-map.component.ts
@@ -8,9 +8,9 @@ import 'rxjs/add/operator/catch'
 import { environment } from '../../environments/environment';
 
 @Component({
-selector: 'app-home-map',
-templateUrl: './home-map.component.html',
-styleUrls: ['./home-map.component.css']
+  selector: 'app-home-map',
+  templateUrl: './home-map.component.html',
+  styleUrls: ['./home-map.component.css']
 })
 export class HomeMapComponent implements OnInit {
   private center: number[] = [-72.6506, 41.5623]
@@ -46,70 +46,36 @@ export class HomeMapComponent implements OnInit {
     return (safetyscore - globalmin) / (globalmax - globalmin);
   }
 
+  private getColor(city) {
+    console.log(city);
+    var score = city.score;
+
+    if (score <= 0 && score >= 0.2) {
+      return "red";
+    }
+    else if (score <= 0.2 && score >= 0.4) {
+      return "orange";
+    }
+    else if (score <= 0.4 && score >= 0.6) {
+      return "yellow";
+    }
+    else if (score <= 0.6 && score >= 0.8) {
+      return "green";
+    }
+    else {
+      return "Chartreuse";
+    }
+  }
+
+  private getWidth(city) {
+    console.log(city)
+    var score = city.score;
+
+    return Math.round(score * 100);
+
+  }
+
   private onResize(event) {
     //console.log(event.target.innerWidth);
   }
-
-constructor(private http: Http, private router: Router) { }
-
-ngOnInit() {
-this.getCities().subscribe((cities) => {
-console.log(cities)
-this.cities = cities;
-}, (error) => {
-console.log(error);
-});
-}
-
-private extractData(res: Response) {
-let body = res.json();
-if (body.error) { throw body.error };
-return body.results || {};
-}
-
-private handleError(error: Response | any) {
-return Observable.throw(error.code);
-}
-
-private getCities(): Observable<any[]> {
-return this.http.get(environment.apiUrl+"cities").map(this.extractData).catch(this.handleError);
-}
-
-private goToCity(city: string) {
-let cityURL: string = encodeURI(city + ", Connecticut");
-this.router.navigate(["/", "city", cityURL]);
-}
-
-private onResize(event) {
-console.log(event.target.innerWidth);
-}
-
-private getColor(city){
-console.log(city);
-var score = city.score;
-
-if(score <= 0 && score >= 0.2){
-return "red";
-}
-else if(score <= 0.2 && score >= 0.4){
-return "orange";
-}
-else if(score <= 0.4 && score >= 0.6){
-return "yellow";
-}
-else if(score <= 0.6 && score >= 0.8){
-return "green";
-}
-else {
-return "Chartreuse";
-}
-}
-
-private getWidth(city){
-console.log(city)
-var score = city.score;
-
-return Math.round(score * 100);
-
-}
 }
