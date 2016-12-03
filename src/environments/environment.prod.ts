0e6f08392276d4a9cbc90690e5fe57099b15cbbc
Michael Solati <mkslt04@gmail.com>
2016-12-03 12:24:46 -0500

    Build off of api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  google: "AIzaSyDiv-EwAt2W9VZA1BJiDcVEhE4vtOYcf8c"
+  google: "AIzaSyDiv-EwAt2W9VZA1BJiDcVEhE4vtOYcf8c",
+  apiUrl: "https://team-scsu.herokuapp.com/api/"
 };
