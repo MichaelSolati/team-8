25135816df4a889f18ea970f5e8b2363a18d5f3e
Michael Solati <mkslt04@gmail.com>
2016-12-03 10:49:54 -0500

    Add markers with popups

--- src/app/home-map/home-map.component.ts
+++ src/app/home-map/home-map.component.ts
@@ -6,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-map.component.css']
 })
 export class HomeMapComponent implements OnInit {
-  private lat: number = 41.5623;
-  private lng: number = -72.6506;
+  private center: number[] = [41.5623, -72.6506]
+  private cities: any[] = [{
+    'city': 'New Haven',
+    'population': 200,
+    'lat': 41.3083,
+    'lng': -72.9279
+  }];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onResize(event) {
+    console.log(event.target.innerWidth);
   }
 
 }
